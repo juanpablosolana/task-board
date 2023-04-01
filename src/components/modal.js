eeb45cf9cfef5dea9d1679b4f745a7ca01a6dceb
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-03-31 22:47:25 -0600

    hotfix: eslint app and document files

--- src/components/modal.js
+++ src/components/modal.js
@@ -5,15 +5,18 @@ function Task ({ setShowModal, modalData }) {
 
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm'>
-      <div className='relative flex flex-col p-4 rounded bg-slate-100 w-96'>
+      <div className='relative flex flex-col w-3/4 p-4 rounded bg-slate-100'>
         <div className='flex justify-between'>
           <h2 className='text-xl font-bold'>{title}</h2>
           <span className='text-gray-400' onClick={() => setShowModal(false)}>X</span>
         </div>
         <div className='flex flex-col gap-4 mt-4'>
           <div className='flex flex-col gap-2'>
-            <label htmlFor='title'>Descripción</label>
+            <label htmlFor='title'>Editar Título</label>
             <input type='text' name='title' id='title' className='p-2 border border-gray-300 rounded' />
+            <button>
+              Actualizar
+            </button>
           </div>
           {comments?.length > 0 &&
             <div className='flex flex-col gap-2'>
@@ -26,7 +29,7 @@ function Task ({ setShowModal, modalData }) {
                       <p className='font-bold'>{item.user.name}</p>
                       <p className='text-xs text-gray-300'> hace 5 horas</p>
                     </div>
-                    <div className='px-3 py-3 mx-6 bg-white'>
+                    <div className='px-3 py-3 mx-12 bg-white'>
                       <p>{item.text}</p>
                     </div>
                   </div>
